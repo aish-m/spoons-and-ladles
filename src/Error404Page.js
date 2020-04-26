7c4a8cea2887f1ccf4422e8f5bf82ba04897d23d
Aishwarya Manjunatha <aishwarya@Aishwaryas-MacBook-Pro.local>
2020-04-26 01:31:55 -0500

    Error 404 page

--- src/Error404Page.js
+++ src/Error404Page.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, {useEffect} from "react";
+import error404 from './images/error-404.gif';
+import {NavLink} from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import './Error404Page.css';
+import mascot404 from './images/mascot-error-404.png';
+import mascot from "./images/mascot.png";
+import text from "./images/snl-text.png";
 
 function Error404Page() {
+    useEffect(() => {
+        document.getElementById("pageHeader").classList.add("loginMode");
+        document.getElementById("pageFooter").classList.add("loginMode");
+    }, []);
+
     return(
-        <div>
-           Error 404!
+        <div className="error-404-div">
+            <div className="header-left-error-404-page">
+                <NavLink to="/">
+                    <img id="error-page-mascot" src={mascot} alt="Spoons & Ladles mascot"/>
+                    <img id="error-page-companyName" src={text} alt="Spoons & Ladles text" />
+                </NavLink>
+            </div>
+            <div id="notFoundText"> Umm, are you sure you're on the right page? </div>
+            <div id="notFoundTextSubtitle"> ERROR 404: Page Not found </div>
+            <div id="404pageImages">
+                {/*<img*/}
+                {/*    src={mascot404}*/}
+                {/*    alt="404 Error GIF"*/}
+                {/*    id="mascotError404"*/}
+                {/*/>*/}
+                <img
+                    src={error404}
+                    alt="404 Error GIF"
+                    className="error-404-gif"
+                />
+            </div>
+           <NavLink to="/" className="nav-links">
+                <Button variant="contained"
+                        id="goBackToSnL"
+                        size="large"
+                >
+                    RETURN TO HOME PAGE
+                </Button>
+            </NavLink>
         </div>
     )
 }
